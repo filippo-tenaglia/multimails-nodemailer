83ac7ebfdb63a6ba472e9e4312934a3503a4e14e
Filippo Tenaglia <filippo.tenaglia@aesystech.it>
2017-10-31 10:10:20 +0100

    Version 0.6

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-This is a simple module to send many e-mails using nodemailer.
+This is a simple module to send many e-mails using nodemailer and retrieve the action when there is a connection error using winston module.
 
 # Send many mails with nodemailer
 
 The purpose of this module is manage a JSON object to send many mails using nodemailer.
 The connection informations are retrieved by dotenv module from .env file.
 Another thing is that if the parameters from and sender don't exist, the module take the user credentials.
+I decide to manage the logging system using Winston module, it seems very flexible! It permits, in a simple manner, to manage the response using different methods.
+The critical errors messages and the sent informations are configurable, for the connection errors I decide to use a file to stored the informations and then retrieve to attempt again the operation.
 
 ## Dependencies
 
@@ -13,6 +15,7 @@ What you need to run this app:
 * [Node.Js](https://nodejs.org)
 * [Nodemailer](https://nodemailer.com)
 * [DotEnv]
+* Winston: https://github.com/winstonjs/winston
 
 ## Instructions
 
@@ -20,9 +23,43 @@ What you need to run this app:
 
 * npm install multimails-nodemailer
 
+# functions' documentation:
 
-## Roadmap
-the next steps will be to modify the logging system to manage different types:
-- file (the default type and already used);
-- nosql database;
-- sql database.
+## send_mail
+this function will expect two arrays that will contains:
+1) a list of objects formatted following this guide: https://nodemailer.com/message/
+example: 
+
+[{from:"me@web.com", to:"you@web.com",subject:"E-mail subject", text:"text message"},
+{to:"you@web.com", subject:"Subject", html:"<b>Hello all</b>"}]
+
+
+2) an array with two objects formatted following this page: https://github.com/winstonjs/winston
+
+example:
+
+const errlog = winston.createLogger({
+  format: winston.format.json(),
+	level:'error',
+  transports: [
+  new winston.transports.File({ filename: 'mail_errors.json', level: 'error' })
+  ]
+});
+const infolog = winston.createLogger({
+  format: winston.format.json(),
+	level:'info',
+  transports: [
+    new winston.transports.File({ filename: 'mail_logs.json', level:'info' })
+  ]
+});
+var loggers=[errlog,infolog];
+
+
+Note: the order isn't casual, the app will try to set error in the first object and info into the second.
+Note 2: the app will create automaticalli also one file to manage the connection's errors named mail_warn.json.
+
+## send_old_mail
+this function read the file mail_warn.json (created with the function send_mail) and rename it to mail_warn.log; then read the file and recreate an array with the e-mail's informations and send it to send_mail passing also an object to set the loggers described previously.
+The function want an array of two objects like the second object passend on send_mail.
+
+The version 1.0.0 will contains two loggers setted in another file or something like that to manage a call withoud these informations.
--- index.js
+++ index.js
@@ -4,7 +4,15 @@ const nodemailer = require('nodemailer');
 const dotenv = require('dotenv');
 dotenv.load();
 var fs = require('fs');
-    
+var array = require('lodash/array');
+var winston=require("winston");
+const warnlog = winston.createLogger({
+  format: winston.format.json(),
+	level:'warn',
+  transports: [
+    new winston.transports.File({ filename: 'mail_warn.json', level: 'warn' })
+  ]
+});
     let transporter = nodemailer.createTransport({
         host: process.env.mail_host,
         port: process.env.mail_port,
@@ -16,45 +24,78 @@ var fs = require('fs');
     });
 
 
-	let send_mail=((mailOptions)=>{
-		var id=Date.now();
-		var accepted=[];
-		var rejected=[];
+	let send_mail=((mailOptions, loggers)=>{
+		var response;
 	for(var i=0; i<mailOptions.length; i++){
 		if(mailOptions[i].from==undefined || mailOptions[i].from==''){
 			mailOptions[i].sender=process.env.mail_user;
 		}
-		var x=0;
+			var x=0;
 		transporter.sendMail(mailOptions[i], (error, info) => {
         if (error) {
-			console.error(error);
-                mailOptions[x].response={
-					'message':error
+if (error.code=='ECONNECTION'){
+				warnlog.log('warn',mailOptions[x]);
+				} else {
+                response={status:403,
+				body:{
+                    'success':false,
+                    'error':{
+                                'code':'403',
+                                'message':error
+                    }
+                }
 				};
-				rejected.push(mailOptions[x]);
-        } else{
-//        console.log('Message sent: %s', info.messageId);
-console.log(info);
-		mailOptions[x].response={
-			'message':'Message sent: %s'+ info.messageId
+				loggers[0].log('error', response);
+				}
+			} else{
+		response={status:200,
+				body:{
+                        'success':true,
+                        'result':'Message sent: %s'+ info.messageId
+		}
 		};
-				accepted.push(mailOptions[x]);
+		loggers[1].log('info',response);
 		}
 		x++;
 		});
 		}
- 		  setTimeout(function () {
-fs.appendFile("./log_"+id+".json", '{rejected:'+JSON.stringify(rejected)+'},\n{accepted:'+JSON.stringify(accepted)+'}', function(err) {
-    if(err) {
-        return console.log(err);
-    }
 
-    console.log("The file was saved!");
-});
-  }, 10000);
 
 });
+
+let send_old_mail=(loggers)=>{
+	var mailOptions=[];
+	var uniqueObjects=[];
+	fs.renameSync('mail_warn.json','mail_warn.log');
+var document=fs.readFileSync('mail_warn.log', 'utf-8', (err, data) => {
+  if (err) throw err;
+return data;
+});
+if(document.length>0){
+//	document=document.replace('{\"level\":\"warn\",\"message\":','');
+//document=document.replace("}}","}");
+	var rows=document.split('\r\n');
+//	console.log(rows);
+	for (var r in rows){
+	var obj=rows[r].replace("}}","}");
+	obj=obj.replace('{\"level\":\"warn\",\"message\":','');
+	if(obj.length>1){
+//	console.log(obj);
+		if(uniqueObjects.indexOf(obj)==-1){
+	uniqueObjects.push(obj);
+	obj=JSON.parse(obj);
+	mailOptions.push(obj);
+}
+	}
+	}
+  setTimeout(function () {
+		  console.log(mailOptions);
+	var msg=send_mail(mailOptions, loggers);
+  }, 2000);
+	}
+};
 
 module.exports = {
-send_mail:send_mail
+send_mail:send_mail,
+send_old_mail:send_old_mail
 };
